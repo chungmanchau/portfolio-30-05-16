f9603bb9db1eb6fa0cb56b52579b03895a10bb27
Chung <chungman1993@gmail.com>
2016-05-30 22:31:08 +0100

    Open panel function added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 $(function() {
-	$('#logo').click(function(){
+	$('#logo, .active').click(function(){
 		toggleMenu();
-	})
+	});
+
+	for (var i = 0; i < 5; i++) {
+		$('.layer' + i).click(function(){
+			active(this.id);
+		});
+	}
 });
 
 function toggleMenu() {
-	$('.layer, .logo').toggleClass("active");
+	if($('.layer').hasClass('active') ){
+		$('.layer').removeClass('active closed');
+		$('.layer').addClass('menu-open');
+		for (var i = 0; i < 5; i++) {
+			$('.logo').removeClass('point' + i);
+		}
+	} 
+	else if($('.layer').hasClass('menu-open')){
+		$('.layer, .logo').removeClass('menu-open');
+	} else {
+		$('.layer, .logo').addClass('menu-open');
+	}
+}
+
+function rotateLogo() {
+	
+}
+
+function active(id) {
+	console.log(id);
+	id = parseInt(id);
+	$('#' + id).addClass('active');
+	$('.logo').addClass('point' + id);
+	var n = id;
+	for (var i = 0; i < 4; i++) {
+		n += 1;
+		if (n === 5) n = 1;
+		if (n === id) continue;
+		$('#'+n).removeClass('menu-open');
+		$('#'+n).addClass('closed');
+	}
 }
