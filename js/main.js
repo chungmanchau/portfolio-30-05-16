aa58c40424944d85f1770ce0303195870f096c1b
Chung <chungman1993@gmail.com>
2016-05-31 22:50:38 +0100

    Panels opening and closing animation sorted

--- js/main.js
+++ js/main.js
@@ -3,16 +3,32 @@ $(function() {
 		toggleMenu();
 	});
 
-	for (var i = 0; i < 5; i++) {
-		$('.layer' + i).click(function(){
-			active(this.id);
-		});
-	}
+	$('.layer1').click(function(){
+		id = parseInt(this.id)
+		contentId = 3;
+		active(id, contentId);
+	});
+	$('.layer2').click(function(){
+		id = parseInt(this.id)
+		contentId = undefined;
+		active(id, contentId);
+	});
+	$('.layer3').click(function(){
+		id = parseInt(this.id)
+		contentId = 1;
+		active(id, contentId);
+	});
+	$('.layer4').click(function(){
+		id = parseInt(this.id)
+		contentId = 2;
+		active(id, contentId);
+	});
 });
 
 function toggleMenu() {
 	if($('.layer').hasClass('active') ){
-		$('.layer').removeClass('active closed');
+		$('.content-layer').removeClass('show-content');
+		$('.layer').removeClass('active closed show-content');
 		$('.layer').addClass('menu-open');
 		for (var i = 0; i < 5; i++) {
 			$('.logo').removeClass('point' + i);
@@ -28,17 +44,22 @@ function toggleMenu() {
 	}
 }
 
-function active(id) {
+function active(id, contentId) {
 	console.log(id);
-	id = parseInt(id);
-	$('#' + id).addClass('active');
+	$('#' + id + ', #' + contentId).addClass('active');
+	$('#' + contentId + ', [layer='+ contentId +']').addClass('show-content');
 	$('.logo').addClass('point' + id);
 	var n = id;
 	for (var i = 0; i < 4; i++) {
 		n += 1;
 		if (n === 5) n = 1;
-		if (n === id) continue;
+		if (n === id || n === contentId) continue;
 		$('#'+n).removeClass('menu-open');
-		$('#'+n).addClass('closed');
+		if(id === 2) {
+			$('#'+n).addClass('');
+		} else {
+			$('#'+n).addClass('closed');
+		}
+		
 	}
 }
